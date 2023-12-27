f52077912e1ed93cf01a1c40538adc22eb63f46c
keshavjatt <thecoder780@gmail.com>
2023-12-27 18:51:09 +0530

    Add GraphQL for users

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,22 @@ import packageRoutes from './routes/packageRoutes';
 import subscriptionRoutes from './routes/subscriptionRoutes';
 import { credentials } from './constants';
 import { errorHandler } from './errorHandlers/globalErrHandler'
+import { graphqlHTTP } from 'express-graphql';
+import { schema } from './graphql/index';
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-//swaggerSetup();
+//Swagger Setup
 const swaggerDocs = YAML.load('src/swagger/Dth_Task.postman_collection.json-OpenApi3Yaml.yaml');
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-//connection to database 
+//GraphQL Setup
+app.use('/graphql', graphqlHTTP({ schema, graphiql:true }))
+
+//Database Conne
 AppDataSource.initialize().then(()=> {
     app.listen(credentials.PORT, ()=> {
     console.log(`Server is running on PORT ${credentials.PORT}`)
@@ -31,7 +36,6 @@ AppDataSource.initialize().then(()=> {
 
 //global error handlers
 app.use(errorHandler)
-
 
 //routes
 app.use("/api/v1/user", userRoutes)
